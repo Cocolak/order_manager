a85b7b6756086a95caa68d1379be45c8923eabc7
Cocolak <cocolak333@gmail.com>
2023-11-16 10:31:45 +0100

    Add pywinstyles

--- test_run.py
+++ test_run.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from mainWindow import Ui_MainWindow
 import sys
+import pywinstyles
 
 
 if __name__ == "__main__":
@@ -8,6 +9,7 @@ if __name__ == "__main__":
     window = QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(window)
+    pywinstyles.change_header_color(window, color="blue")
     window.show()
     app.exec_()
     
